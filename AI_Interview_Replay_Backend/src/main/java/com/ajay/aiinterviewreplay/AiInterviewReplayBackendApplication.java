6d596ccf629a62a47d27e1950f504b4994c5df66
Ajay Sontakke <sontakkeajay05@gmail.com>
2026-08-18 22:11:27 +0530

    Initial backend project setup

---
+++ AI_Interview_Replay_Backend/src/main/java/com/ajay/aiinterviewreplay/AiInterviewReplayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.aiinterviewreplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewReplayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewReplayBackendApplication.class, args);
+	}
+
+}
